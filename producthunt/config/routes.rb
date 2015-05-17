f09c1a4a0db39d8e7f296db8cc443c21fbd2095d
kennygeiler <kennethlgeiler@gmail.com>
2015-05-17 12:32:55 -0400

    add routes

--- producthunt/config/routes.rb
+++ producthunt/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :user
+  resources :posts do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
