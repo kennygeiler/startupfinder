0d73a224295a4eb60429aa7f37a858e6718dbe53
kennygeiler <kennethlgeiler@gmail.com>
2015-05-17 12:08:01 -0400

    add columns to user migartion

--- producthunt/db/migrate/20150517155707_create_users.rb
+++ producthunt/db/migrate/20150517155707_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
-      t.timestamps null: false
+      t.string :username, unique: true, null: false
+      t.string :password_digest, null: false
+      t.text :credentials
+      t.timestamps
     end
   end
 end
