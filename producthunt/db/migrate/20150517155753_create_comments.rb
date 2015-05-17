35eb1dbeb512510cec5585c45fb73d0c829627e7
kennygeiler <kennethlgeiler@gmail.com>
2015-05-17 12:10:32 -0400

    add columms to comment migration

--- producthunt/db/migrate/20150517155753_create_comments.rb
+++ producthunt/db/migrate/20150517155753_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+      t.references :user
+      t.text :content
       t.timestamps null: false
     end
   end
