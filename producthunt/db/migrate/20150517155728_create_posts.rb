66ea63f6e9b248a63cbde2778b2af2c416ea5a16
kennygeiler <kennethlgeiler@gmail.com>
2015-05-17 12:09:52 -0400

    add columns to post migration

--- producthunt/db/migrate/20150517155728_create_posts.rb
+++ producthunt/db/migrate/20150517155728_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :title, null: false
+      t.string :link, null: false
+      t.references :user
       t.timestamps null: false
     end
   end
