5da3869a4fae6e0128d52da7733dadbc608b971c
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 10:40:00 -0400

    fix login bug

--- producthunt/app/controllers/sessions_controller.rb
+++ producthunt/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in @user
+      log_in user
       redirect_to posts_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
