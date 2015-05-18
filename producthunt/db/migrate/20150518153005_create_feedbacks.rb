f33a8b19959e0e0f3bc1cc2eab2d5134b7cda3e8
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 11:47:25 -0400

    update migration to include post id

--- producthunt/db/migrate/20150518153005_create_feedbacks.rb
+++ producthunt/db/migrate/20150518153005_create_feedbacks.rb
@@ -2,6 +2,7 @@ class CreateFeedbacks < ActiveRecord::Migration
   def change
     create_table :feedbacks do |t|
       t.string :question
+      t.references :post
       t.timestamps null: false
     end
   end
