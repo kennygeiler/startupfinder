a6c701995215e46cde8e0c5cc4956e15112eff06
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 12:12:49 -0400

    almost there

--- producthunt/db/schema.rb
+++ producthunt/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517160506) do
+ActiveRecord::Schema.define(version: 20150518153005) do
 
   create_table "badges_sashes", force: :cascade do |t|
     t.integer  "badge_id"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150517160506) do
   add_index "badges_sashes", ["badge_id", "sash_id"], name: "index_badges_sashes_on_badge_id_and_sash_id"
   add_index "badges_sashes", ["badge_id"], name: "index_badges_sashes_on_badge_id"
   add_index "badges_sashes", ["sash_id"], name: "index_badges_sashes_on_sash_id"
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string   "question"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "merit_actions", force: :cascade do |t|
     t.integer  "user_id"
