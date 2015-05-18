3fddfd187e6d39307e892cc1af0092c98ff95272
kennygeiler <kennethlgeiler@gmail.com>
2015-05-17 20:19:29 -0400

    show method and view working

--- producthunt/app/controllers/posts_controller.rb
+++ producthunt/app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
 end
