5f1c1605ed1314726560adf183f3028921e82337
kennygeiler <kennethlgeiler@gmail.com>
2015-05-17 20:38:11 -0400

    create method added

--- producthunt/app/controllers/posts_controller.rb
+++ producthunt/app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -13,11 +18,17 @@ class PostsController < ApplicationController
   end
 
   def show
-
+    #this will be the feedback area
   end
 
   def destroy
 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :user_id, :hiring)
   end
 
 end
