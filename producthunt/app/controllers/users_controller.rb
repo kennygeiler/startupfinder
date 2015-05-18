680f0be9cdf9fc957dc8372e589cc380978bf21c
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 09:38:08 -0400

    add user to new page

--- producthunt/app/controllers/users_controller.rb
+++ producthunt/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -7,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      log_in(@user)
       flash[:success] = "Welcome to the Sample App!"
       redirect_to posts_path
     else
