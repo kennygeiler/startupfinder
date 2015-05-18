47c1e6b48b088d2076b3d055e7da98856ee0eb70
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 13:37:41 -0400

    add nested resource feedbacks to posts?:

--- producthunt/config/routes.rb
+++ producthunt/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :posts do
-    resources :comments
+    resources :feedbacks
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
