66209e2ad672f1ae565f70cdd73e51464b2c075d
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 11:46:34 -0400

    add new and create methods

--- producthunt/app/controllers/feedbacks_controller.rb
+++ producthunt/app/controllers/feedbacks_controller.rb
@@ -1,2 +1,20 @@
 class FeedbacksController < ApplicationController
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      redirect_to feedbacks_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:question, :post_id)
+  end
 end
