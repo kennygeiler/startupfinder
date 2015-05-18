d0e8bbfffb361027733fb1956ed279aab2898b15
kennygeiler <kennethlgeiler@gmail.com>
2015-05-18 18:01:13 -0400

    add new and creat methods

---
+++ producthunt/app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @feedback = Feedback.find(params[:feedback_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id, :feedback_id)
+  end
+end
