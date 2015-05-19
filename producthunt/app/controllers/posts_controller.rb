9776d2c62c965028ea532f9e1a02325f1db3ebc1
kennygeiler <kennethlgeiler@gmail.com>
2015-05-19 18:11:35 -0400

    add description to post params

--- producthunt/app/controllers/posts_controller.rb
+++ producthunt/app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :link, :user_id, :hiring, :staff_pick)
+    params.require(:post).permit(:title, :link, :user_id, :hiring, :staff_pick, :description)
   end
 
 end
