9832cdfd909d2984701973db73be0428dc9e86f6
kennygeiler <kennethlgeiler@gmail.com>
2015-05-19 11:18:55 -0400

    add vote weight calculation method based on karma

--- producthunt/app/models/user.rb
+++ producthunt/app/models/user.rb
@@ -9,4 +9,14 @@ class User < ActiveRecord::Base
     update_attribute(:karma, karma + count)
   end
 
+  def vote_weight_score
+    if self.karma < 50
+      return 1
+    elsif 50 < self.karma && self.karma < 150
+      return 3
+    else
+      return 5
+    end
+  end
+
 end
