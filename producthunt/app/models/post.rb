3b8b73e4e1c9958978449a23d7170c598257dc14
kennygeiler <kennethlgeiler@gmail.com>
2015-05-21 09:37:10 -0400

    add day week month methods

--- producthunt/app/models/post.rb
+++ producthunt/app/models/post.rb
@@ -8,4 +8,16 @@ class Post < ActiveRecord::Base
     self.get_upvotes.sum(:vote_weight)
   end
 
+  def self.today
+    where("updated_at >= ?", Time.zone.now.beginning_of_day)
+  end
+
+  def self.week
+    where("updated_at >= ?", Time.zone.now.beginning_of_week)
+  end
+
+  def self.month
+    where("updated_at >= ?", Time.zone.now.beginning_of_month)
+  end
+
 end
