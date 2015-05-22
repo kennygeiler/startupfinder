030a28ab16011a1db514c8dc55d7db67c2e95b90
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-22 11:26:20 -0400

    adding questions with ajax, still need to hide new comment form

--- producthunt/app/assets/javascripts/feedbacks.js
+++ producthunt/app/assets/javascripts/feedbacks.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
     event.preventDefault();
 
     $form = $(event.target);
-    $destination = 
 
     $.ajax({
       url: $form.attr('action'),
@@ -33,6 +32,22 @@ $(document).ready(function() {
     $target = $(event.target);
     $target.closest('.feedback-listing-container').children('.new-feedback-form').show();
     $target.hide();
+  })
+
+  $('.container').on('submit', '.new_feedback', function(event) {
+    event.preventDefault();
+
+    $form = $(event.target);
+
+    $.ajax({
+      url: $form.attr('action'),
+      type: 'POST',
+      data: $form.serialize()
+    }).then(function(response) {
+      $form.closest('.feedback-listing-container').children('.feedback-listing').append(response);
+      debugger
+      $form.trigger('reset')
+    })
   })
 
   $('.container').on('click', '.comment-upvote-button', function(event) {
