b987801a2dd94e3280147073dc4d1ef122980d39
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-21 20:00:46 -0400

    vote ajax call updating vote count

--- producthunt/app/assets/javascripts/feedbacks.js
+++ producthunt/app/assets/javascripts/feedbacks.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
 
   $('.container').on('click', '.comment-upvote-button', function(event) {
+
     event.preventDefault();
+    event.stopPropagation();
 
     $target = $(event.target);
-    path = $target.closest('.comment.upvote').children('a').attr('href');
+    path = $target.closest('.comment-upvote').children('a').attr('href');
+    $destination = $target.closest('.comment-upvote').children('.comment-score')
 
     $.ajax({
       url: path,
       type: 'PUT'
+    }).then(function(response) {
+      $destination.html(response)
     })
   })
 
