1f95bd4948ea9fcd355e2cec1bfb909563e5e831
kennygeiler <kennethlgeiler@gmail.com>
2015-05-22 11:55:30 -0400

    changes

--- producthunt/app/assets/javascripts/posts.js
+++ producthunt/app/assets/javascripts/posts.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
   $('.container').on('click', '.post-filter', function(event) {
     event.preventDefault();
-    
+
     var $target = $(event.target);
 
     $.get($target.attr('href'), function(response) {
