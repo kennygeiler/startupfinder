5a6f8ba0008fea31cfdaf3071f9ebb38da5faa29
kennygeiler <kennethlgeiler@gmail.com>
2015-05-21 20:11:51 -0400

    seed data with some accepted posts and some not

--- producthunt/db/seeds.rb
+++ producthunt/db/seeds.rb
@@ -5,24 +5,21 @@ Post.create(title: 'guuco', link: 'http://www.yahoo.com', description: Faker::Lo
 Post.create(title: 'yolo', link: 'http://www.face.com', description: Faker::Lorem.sentence, hiring: false, staff_pick: false, user_id: 2)
 Post.create(title: 'turnupo', link: 'http://www.nppl.com', description: Faker::Lorem.sentence, hiring: true, staff_pick: true, user_id: 2)
 
-20.times do
-  post = Post.create(title: Faker::Company.name, link: Faker::Internet.url, description: Faker::Lorem.sentence, hiring: true, staff_pick: true, user_id: 1)
-  5.times do
-    feedback = post.feedbacks.create(question: Faker::Lorem.sentence)
-    5.times do
-      feedback.comments.create(content: Faker::Lorem.paragraph, user_id: 1)
-    end
-  end
+
+10.times do
+  Post.create(title: Faker::Company.name, link: Faker::Internet.url, description: Faker::Lorem.sentence, hiring: true, staff_pick: true, user_id: 1)
 end
 
-20.times do
-  post = Post.create(title: Faker::Company.name, link: Faker::Internet.url, description: Faker::Lorem.sentence, hiring: true, staff_pick: false, user_id: 1)
-  5.times do
-    feedback = post.feedbacks.create(question: Faker::Lorem.sentence)
-    5.times do
-      feedback.comments.create(content: Faker::Lorem.paragraph, user_id: 1)
-    end
-  end
+10.times do
+  Post.create(title: Faker::Company.name, link: Faker::Internet.url, description: Faker::Lorem.sentence, hiring: true, staff_pick: true, user_id: 1, accepted: true)
+end
+
+10.times do
+  Post.create(title: Faker::Company.name, link: Faker::Internet.url, description: Faker::Lorem.sentence, hiring: true, staff_pick: false, user_id: 1)
+end
+
+10.times do
+  Post.create(title: Faker::Company.name, link: Faker::Internet.url, description: Faker::Lorem.sentence, hiring: true, staff_pick: false, user_id: 1, accepted: true)
 end
 
 User.create(name: 'kenny')
