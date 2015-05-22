d224bfd684cd2cdef4a1687c5355d020084ef34a
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-21 20:16:25 -0400

    update karma count on the fly when voted for

--- producthunt/app/assets/javascripts/feedbacks.js
+++ producthunt/app/assets/javascripts/feedbacks.js
@@ -5,15 +5,20 @@ $(document).ready(function() {
     event.preventDefault();
     event.stopPropagation();
 
-    $target = $(event.target);
-    path = $target.closest('.comment-upvote').children('a').attr('href');
-    $destination = $target.closest('.comment-upvote').children('.comment-score')
+    var $target = $(event.target);
+    var path = $target.closest('.comment-upvote').children('a').attr('href');
+    var $voteDestination = $target.closest('.comment-upvote').children('.comment-score')
+    var userId = $target.closest('.feedback-comment').find('.user-karma').data('userId')
 
     $.ajax({
       url: path,
       type: 'PUT'
     }).then(function(response) {
-      $destination.html(response)
+      respArray = response.split('|');
+      var voteCount = respArray[0];
+      var karmaCount = respArray[1];
+      $voteDestination.html(voteCount);
+      $('*[data-user-id="' + userId + '"]').html(karmaCount);
     })
   })
 
